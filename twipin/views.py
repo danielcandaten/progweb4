309de272f31990fedb30a06a406666ed2ad81699
ttomazoni.github <tomazoni89@gmail.com>
2018-10-10 20:45:14 -0300

    Daniel alterou banco

--- twipin/views.py
+++ twipin/views.py
@@ -2,35 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from .models import Twip
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
    contexto = {} 
    return render(request, 'twipin/index.html', contexto)
 
-def json_listar_twips (request):
-   twips = Twip.objects.all()
-   jt = {"lista":[]}
-   for t in twips:
-      temp = {"id":t.id, "texto": t.texto, "autor":t.autor.username}
-      jt["lista"].append(temp) 
-   return JsonResponse(jt)
+@csrf_exempt
+def api_twips (request):
+   if request.method=="GET":
+      twips = Twip.objects.all()
+      jt = {"lista":[]}
+      for t in twips:
+         temp = {"id":t.id, "texto": t.texto, "autor":t.autor.username}
+         jt["lista"].append(temp) 
+      return JsonResponse(jt)
+   elif request.method == "POST":
+      #salva no banco
+      t = request.POST["texto"]
+      usuario = User.objects.get(username='admin')
+      twip = Twip(autor=usuario, texto = t)
+      twip.save()
 
-
-def json_exemplo(request):
-   
-   
-
-   pessoa = {
-       "nome": "João",
-       "idade": 18,
-       "parentes": {
-           "mãe": "Maria",
-           "pai": "Pedro",
-           "irmãos": 2
-       }
-   }
-
-   return JsonResponse(pessoa)
+      jt = {"mensagem":"ok"}
+      return JsonResponse(jt)
 
 
     
